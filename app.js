133e3f961a1ac751f64cf363ed626c00f3f1db63
Andy Knolla <andyknolla@gmail.com>
2016-07-20 23:25:20 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var app = angular.module("reddit", [])
+
+app.controller("powerController", function($scope) {
+    $scope.places = [{
+        title: 'Lumpy Ridge',
+        author: 'Bernard Gillett',
+        image: 'http://i20.photobucket.com/albums/b226/vivid_one/Hiking/Lumpy%20Ridge%20Feb%202009/21909_Lumpy_Ridge13x.jpg',
+        description: 'Located just north of Estes Park, Lumpy Ridge consists of over three miles of granite domes, cliffs, and outcrops. The Ridge overlooks the lower reaches of Black Canyon, site of the historic MacGregor Ranch. This ranch has been a local landmark since the 1880s, and its lush grasslands, together with the mountains of the Front Range, provide beautiful, panoramic scenery for climbers to enjoy.\n\t With several hundred routes to choose from, Lumpy ridge is the most heavily visited climbing area in Estes Valley. It is best known for excellent crack climbing, but face climbs abound, and bolts amply protect the newer routes. The majority of the rocks recline at about a 80-degree pitch, and generally the climbing requires refined technique instead of brute force. The major cliffs face south and recieve sun all day long.]',
+        votes: 10,
+        comments: []
+    }, {
+        title: 'Cathedral Spires',
+        author: 'Nick Casamata',
+        image: 'http://www.summitpost.org/images/large/187135.JPG',
+        description: 'The slender, spire-like nature of the Cynical Pinnacle stands resolute on the hillside, serving as a lodestar and casting out a beacon of light to wayward climbers, guiding them to perhaps the best 5.9 and 5.11 multipitch jam cracks in the state. Further west, Poe Buttress and Sunshine Wall wait with even more ultra-classic multipitch routes from 5.9 to 5.12. Splitter cracks up vertical walls not your thing? Head even further west and check out The Dome and its smooth, friction-dependent slabs that are unmatched and unparalleled by anything outside of Tuolumne Meadows in California.',
+        votes: 3,
+        comments: []
+    }, {
+        title: 'Eldorado Canyon',
+        author: 'Steve Levin',
+        image: 'http://www.michaeldeleonphoto.com/wp-content/uploads/2013/08/20130826_024-Edit.jpg',
+        description: 'Eldorado Canyon is one of the great American rockclimbing areas. Cut through the uplifted Flatirons front southwest of Boulder, Colorado, the canyons\'s sheer walls rise hundreds of feet up steep slopes on either side of the cascading South Boulder Creek, offering a broad range of climbing routes, from single-pitch cragging to multi-pitch adventure climbing. The canyon, preserved as a state park, is unspoiled and extraordinarily beautiful. Presenting a complex rock architecture of iconic cliff formations, the steep sandstone walls are a lichen-streaked palette of color, ranging from bright orange, vermillion, and maroon red, to yellow and lime green, to black and gray. Pigeons and swifts soar through the sky, the creek crashes down its steep course, and climbers dot the high walls. With something for every climber, from 5.6 "moderates", to classic 5.9 and 5.10 climbs, to extreme high-end challenges, Eldorado is a diverse, beautiful, rewarding place to climb.',
+        votes: 8,
+        comments: []
+    }, {
+        title: 'Black Canyon of the Gunnison',
+        author: 'Robbie Williams',
+        image: 'http://www.visitmontrose.com/ImageRepository/Path?filePath=%2FDocuments%5CContent%5C24%5C32%2FBlack+Canyon+4_201312121425412074.jpg',
+        description: 'Before the birds sing, you awake. Rise in the dark with mindfulness and purpose. In the darkness, breakfast is hurried. Then, when the sun is born on the crest of the horizon, you grab gear and rope and water and candy bars. The shadows of the junipers are long, cast toward you across the dry ground. You and  your partner run through trees and scrub oak and yucca to the opening of a narrow defile, the gash of a gully, the descent, the start.\n Success is not guaranteed. Can you escape from the depths by sunset? By midafternoon, the shadows that mark the passing of the hours have crept up the cliffs on the opposite rim. And the realization hits: that you have no choice but to continue--that it is easier to keep climbing, upward to safety, to regain the sanctuary of the juniper trees on the plateua-- than to fail, to rappel off, to retreat, to thrash back up that dreaded descent gully. Little did you  know that the die was cast hours, perhaps even days ago, when you finalized your plans to come to the canyon -- that you must continue your climb. But now as daylight wanes, your worn down muscles and adrenaline-fed thoughts know it. You are completely committed to finishing your ascent.',
+        votes: 4,
+        comments: []
+    }]
+})
